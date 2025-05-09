571001313af1e145a1e498648a239baae8717db0
Anshuman Shukla <anshuman.gomicro@gmail.com>
2025-05-09 20:10:27 +0530

    Create configuration for backend API connections

---
+++ backend-service/src/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    stockAPIBaseURL: 'http://20.244.56.144/evaluation-service',
+    credentials: {
+        email: "anshuman.2226cseml10@kiet.edu",
+        name: "anshuman shukla",
+        rollNo: "2200291530021",
+        accessCode: "SxVeja",
+        clientID: "0673a3b8-574d-4421-bfb4-99f01c017c93",
+        clientSecret: "yfvtXQMRCYyREjHh"
+    },
+    cacheTTL: 60
+};
